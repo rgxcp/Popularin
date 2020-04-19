87b471d8f84ddaa8391d575eeeb6bbaa5c680119
Rommy Gustiawan <rgcppp@gmail.com>
2020-04-19 09:51:17 +0700

    Membuat kerangka logic fragment home

--- app/src/main/java/xyz/fairportstudios/popularin/fragments/HomeFragment.java
+++ app/src/main/java/xyz/fairportstudios/popularin/fragments/HomeFragment.java
@@ -1,21 +1,58 @@
 package xyz.fairportstudios.popularin.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import xyz.fairportstudios.popularin.R;
+import xyz.fairportstudios.popularin.activities.FilmListActivity;
 
 public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View mView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageView mAction = mView.findViewById(R.id.ctg_action);
+        ImageView mDrama = mView.findViewById(R.id.ctg_drama);
+        ImageView mFantasy = mView.findViewById(R.id.ctg_fantasy);
+
+        mAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mGotoFilmList = new Intent(getActivity(), FilmListActivity.class);
+                mGotoFilmList.putExtra("GENRE_ID", 28);
+                startActivity(mGotoFilmList);
+            }
+        });
+
+        mDrama.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mGotoFilmList = new Intent(getActivity(), FilmListActivity.class);
+                mGotoFilmList.putExtra("GENRE_ID", 18);
+                startActivity(mGotoFilmList);
+            }
+        });
+
+        mFantasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mGotoFilmList = new Intent(getActivity(), FilmListActivity.class);
+                mGotoFilmList.putExtra("GENRE_ID", 14);
+                startActivity(mGotoFilmList);
+            }
+        });
+
+        return mView;
     }
 }
