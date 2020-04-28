fc9a0cf03bdb5a33ac96dbbfcbaf1321443af7b3
Rommy Gustiawan <rgcppp@gmail.com>
2020-04-28 18:22:19 +0700

    Membuat kerangka logic untuk activity splash

---
+++ app/src/main/java/xyz/fairportstudios/popularin/activities/SplashActivity.java
@@ -0,0 +1,35 @@
+package xyz.fairportstudios.popularin.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import xyz.fairportstudios.popularin.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Status bar transparent
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        // Pindah activity otomatis
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent gotoMain = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(gotoMain);
+                finish();
+            }
+        }, 1000);
+    }
+}
