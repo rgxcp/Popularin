7f57aea4b677492381b11742b4d72c8d30385976
Rommy Gustiawan <rgcppp@gmail.com>
2020-04-28 17:28:21 +0700

    Membuat kerangka projek

---
+++ app/src/main/java/xyz/fairportstudios/popularin/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package xyz.fairportstudios.popularin.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import xyz.fairportstudios.popularin.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
