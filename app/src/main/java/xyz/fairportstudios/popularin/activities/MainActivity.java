7cb4e9c47d68e1448ef35913071c5268c5c6e1f0
Rommy Gustiawan <rgcppp@gmail.com>
2020-04-29 12:34:49 +0700

    Refaktor logic untuk sign out

--- app/src/main/java/xyz/fairportstudios/popularin/activities/MainActivity.java
+++ app/src/main/java/xyz/fairportstudios/popularin/activities/MainActivity.java
@@ -7,16 +7,19 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import xyz.fairportstudios.popularin.R;
+import xyz.fairportstudios.popularin.apis.popularin.SignOutRequest;
 import xyz.fairportstudios.popularin.preferences.Auth;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -66,8 +69,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(gotoSignIn);
                 break;
             case R.id.menu_nd_signout:
+                // Menyimpan data
                 Auth auth = new Auth(this);
-                auth.delAuth();
+                String id = auth.getAuthID();
+                String token = auth.getAuthToken();
+
+                // Mengirim data
+                SignOutRequest signOutRequest = new SignOutRequest(
+                        this,
+                        id,
+                        token);
+
+                // Mendapatkan hasil
+                signOutRequest.sendRequest(new SignOutRequest.JSONCallback() {
+                    @Override
+                    public void onSuccess(JSONObject response) {
+                        try {
+                            int status = response.getInt("status");
+
+                            if (status == 525) {
+                                Auth auth = new Auth(MainActivity.this);
+                                auth.delAuth();
+                            } else if (status == 616) {
+                                String errorMessage = response.getString("message");
+                                Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Ada kesalahan dalam database", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException error) {
+                            error.printStackTrace();
+                        }
+                    }
+                });
+
                 drawer.closeDrawer(GravityCompat.START);
                 break;
         }
---
+++ app/src/main/java/xyz/fairportstudios/popularin/apis/popularin/SignOutRequest.java
@@ -0,0 +1,64 @@
+package xyz.fairportstudios.popularin.apis.popularin;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignOutRequest {
+    private Context context;
+    private String id;
+    private String token;
+
+    public SignOutRequest(Context context, String id, String token) {
+        this.context = context;
+        this.id = id;
+        this.token = token;
+    }
+
+    public interface JSONCallback {
+        void onSuccess(JSONObject response);
+    }
+
+    public void sendRequest(final JSONCallback callback) {
+        String requestURL = PopularinBaseRequest.SIGN_OUT;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, requestURL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    callback.onSuccess(jsonObject);
+                } catch (JSONException error) {
+                    error.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("auth_uid", id);
+                headers.put("auth_token", token);
+                return headers;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
+    }
+}
