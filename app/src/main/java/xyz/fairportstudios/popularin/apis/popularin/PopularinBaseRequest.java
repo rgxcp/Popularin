edd171d794865ec7c978af8b68f8fc6da8042be5
Rommy Gustiawan <rgcppp@gmail.com>
2020-04-30 11:10:03 +0700

    Menambahkan static url request untuk Popularin

--- app/src/main/java/xyz/fairportstudios/popularin/apis/popularin/PopularinBaseRequest.java
+++ app/src/main/java/xyz/fairportstudios/popularin/apis/popularin/PopularinBaseRequest.java
@@ -1,7 +1,14 @@
 package xyz.fairportstudios.popularin.apis.popularin;
 
 class PopularinBaseRequest {
+    static final String ADD_COMMENT = "https://popularin.fairportstudios.xyz/api/comment";
+    static final String ADD_FAVORITE = "https://popularin.fairportstudios.xyz/api/favorite";
+    static final String ADD_FILM = "https://popularin.fairportstudios.xyz/api/film";
+    static final String ADD_REVIEW = "https://popularin.fairportstudios.xyz/api/review";
+    static final String ADD_WATCHLIST = "https://popularin.fairportstudios.xyz/api/watchlist";
     static final String REVIEW = "https://popularin.fairportstudios.xyz/api/reviews";
+    static final String SEARCH_USER = "https://popularin.fairportstudios.xyz/api/user/search";
+    static final String SELF = "https://popularin.fairportstudios.xyz/api/user/self";
     static final String SIGN_UP = "https://popularin.fairportstudios.xyz/api/user/signup";
     static final String SIGN_IN = "https://popularin.fairportstudios.xyz/api/user/signin";
     static final String SIGN_OUT = "https://popularin.fairportstudios.xyz/api/user/signout";
