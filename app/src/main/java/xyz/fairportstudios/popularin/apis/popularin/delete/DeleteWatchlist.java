0d0b97f206916331b55c870af6ff5780bf482c15
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-12 21:56:22 +0700

    Mengimplementasi fitur menghapus watchlist

--- app/src/main/java/xyz/fairportstudios/popularin/apis/popularin/delete/DeleteWatchlist.java
+++
@@ -1,9 +0,0 @@
-package xyz.fairportstudios.popularin.apis.popularin.delete;
-
-import xyz.fairportstudios.popularin.apis.popularin.PopularinAPI;
-
-public class DeleteWatchlist {
-    private String id;
-
-    String requestURL = PopularinAPI.WATCHLIST + "/" + id;
-}
--- app/src/main/java/xyz/fairportstudios/popularin/fragments/FilmStatusModal.java
+++ app/src/main/java/xyz/fairportstudios/popularin/fragments/FilmStatusModal.java
@@ -19,6 +19,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import xyz.fairportstudios.popularin.R;
 import xyz.fairportstudios.popularin.activities.EmptyUserActivity;
+import xyz.fairportstudios.popularin.apis.popularin.delete.DeleteWatchlistRequest;
 import xyz.fairportstudios.popularin.apis.popularin.get.FilmSelfRequest;
 import xyz.fairportstudios.popularin.apis.popularin.post.AddWatchlistRequest;
 import xyz.fairportstudios.popularin.models.FilmSelf;
@@ -127,6 +128,19 @@ public class FilmStatusModal extends BottomSheetDialogFragment {
                             @Override
                             public void onError() {
                                 Toast.makeText(context, R.string.add_watchlist_error, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        DeleteWatchlistRequest deleteWatchlistRequest = new DeleteWatchlistRequest(context, filmID);
+                        deleteWatchlistRequest.sendRequest(new DeleteWatchlistRequest.APICallback() {
+                            @Override
+                            public void onSuccess() {
+                                Toast.makeText(context, title + R.string.watchlist_removed, Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onError() {
+                                Toast.makeText(context, R.string.remove_watchlist_error, Toast.LENGTH_SHORT).show();
                             }
                         });
                     }
