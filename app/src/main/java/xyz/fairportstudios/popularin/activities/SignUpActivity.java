4a4f0b153173f386d4d44aa3598f4890987f289f
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-19 12:43:38 +0700

    Refaktor logic activity sign up

--- app/src/main/java/xyz/fairportstudios/popularin/activities/SignUpActivity.java
+++ app/src/main/java/xyz/fairportstudios/popularin/activities/SignUpActivity.java
@@ -1,13 +1,13 @@
 package xyz.fairportstudios.popularin.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
@@ -20,7 +20,7 @@ import xyz.fairportstudios.popularin.preferences.Auth;
 
 public class SignUpActivity extends AppCompatActivity {
     private Context context;
-    private CoordinatorLayout layout;
+    private LinearLayout layout;
     private TextInputEditText inputFirstName;
     private TextInputEditText inputLastName;
     private TextInputEditText inputUsername;
@@ -35,47 +35,61 @@ public class SignUpActivity extends AppCompatActivity {
         // Binding
         context = SignUpActivity.this;
         layout = findViewById(R.id.layout_asu_anchor);
-        inputFirstName = findViewById(R.id.text_asu_first_name);
-        inputLastName = findViewById(R.id.text_asu_last_name);
-        inputUsername = findViewById(R.id.text_asu_username);
-        inputEmail = findViewById(R.id.text_asu_email);
-        inputPassword = findViewById(R.id.text_asu_password);
-        Button buttonSignUp = findViewById(R.id.button_asu_signup);
+        inputFirstName = findViewById(R.id.input_asu_first_name);
+        inputLastName = findViewById(R.id.input_asu_last_name);
+        inputUsername = findViewById(R.id.input_asu_username);
+        inputEmail = findViewById(R.id.input_asu_email);
+        inputPassword = findViewById(R.id.input_asu_password);
+        Button buttonSignUp = findViewById(R.id.button_asu_sign_up);
 
         // Activity
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Menyimpan data
-                String firstName = Objects.requireNonNull(inputFirstName.getText()).toString();
-                String lastName = Objects.requireNonNull(inputLastName.getText()).toString();
-                String username = Objects.requireNonNull(inputUsername.getText()).toString();
-                String email = Objects.requireNonNull(inputEmail.getText()).toString();
-                String password = Objects.requireNonNull(inputPassword.getText()).toString();
+                signUp();
+            }
+        });
+    }
 
-                // POST
-                SignUpRequest signUpRequest = new SignUpRequest(context, firstName, lastName, username, email, password);
-                signUpRequest.sendRequest(new SignUpRequest.APICallback() {
-                    @Override
-                    public void onSuccess(String id, String token) {
-                        Auth auth = new Auth(context);
-                        auth.setAuth(id, token);
+    private void signUp() {
+        // Menyimpan input
+        String firstName = Objects.requireNonNull(inputFirstName.getText()).toString();
+        String lastName = Objects.requireNonNull(inputLastName.getText()).toString();
+        String username = Objects.requireNonNull(inputUsername.getText()).toString();
+        String email = Objects.requireNonNull(inputEmail.getText()).toString();
+        String password = Objects.requireNonNull(inputPassword.getText()).toString();
 
-                        Intent gotoMain = new Intent(context, MainActivity.class);
-                        startActivity(gotoMain);
-                        finishAffinity();
-                    }
+        // Membuat objek
+        SignUpRequest signUpRequest = new SignUpRequest(
+                context,
+                firstName,
+                lastName,
+                username,
+                email,
+                password
+        );
 
-                    @Override
-                    public void onFailed(String message) {
-                        Snackbar.make(layout, message, Snackbar.LENGTH_LONG).show();
-                    }
+        // Mengirim request
+        signUpRequest.sendRequest(new SignUpRequest.APICallback() {
+            @Override
+            public void onSuccess(String id, String token) {
+                // Menyimpan pref dalam storage lokal
+                Auth auth = new Auth(context);
+                auth.setAuth(id, token);
 
-                    @Override
-                    public void onError() {
-                        Snackbar.make(layout, R.string.failed_sign_up, Snackbar.LENGTH_LONG).show();
-                    }
-                });
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+                finishAffinity();
+            }
+
+            @Override
+            public void onFailed(String message) {
+                Snackbar.make(layout, message, Snackbar.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError() {
+                Snackbar.make(layout, R.string.sign_up_error, Snackbar.LENGTH_LONG).show();
             }
         });
     }
