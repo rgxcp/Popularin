ce600fabc589e2e8734a03a8832b32845d6394b3
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-19 23:23:29 +0700

    Refaktor logic fragment edit password

--- app/src/main/java/xyz/fairportstudios/popularin/fragments/EditPasswordFragment.java
+++ app/src/main/java/xyz/fairportstudios/popularin/fragments/EditPasswordFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
@@ -21,6 +22,7 @@ import xyz.fairportstudios.popularin.R;
 import xyz.fairportstudios.popularin.apis.popularin.put.UpdatePasswordRequest;
 
 public class EditPasswordFragment extends Fragment {
+    private Button buttonSave;
     private Context context;
     private CoordinatorLayout layout;
     private TextInputEditText inputCurrentPassword;
@@ -34,47 +36,68 @@ public class EditPasswordFragment extends Fragment {
 
         // Binding
         context = getActivity();
+        buttonSave = view.findViewById(R.id.button_fepw_save);
         layout = view.findViewById(R.id.layout_fepw_anchor);
-        inputCurrentPassword = view.findViewById(R.id.text_fepw_current_password);
-        inputNewPassword = view.findViewById(R.id.text_fepw_new_password);
-        inputConfirmPassword = view.findViewById(R.id.text_fepw_confirm_password);
-        Button buttonSave = view.findViewById(R.id.button_fepw_save);
+        inputCurrentPassword = view.findViewById(R.id.input_fepw_current_password);
+        inputNewPassword = view.findViewById(R.id.input_fepw_new_password);
+        inputConfirmPassword = view.findViewById(R.id.input_fepw_confirm_password);
 
         // Activity
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Menyimpan data
-                String currentPassword = Objects.requireNonNull(inputCurrentPassword.getText()).toString();
-                String newPassword = Objects.requireNonNull(inputNewPassword.getText()).toString();
-                String confirmPassword = Objects.requireNonNull(inputConfirmPassword.getText()).toString();
-
-                // PUT
-                UpdatePasswordRequest updatePasswordRequest = new UpdatePasswordRequest(context, currentPassword, newPassword, confirmPassword);
-                updatePasswordRequest.sendRequest(new UpdatePasswordRequest.APICallback() {
-                    @Override
-                    public void onSuccess() {
-                        Objects.requireNonNull(getFragmentManager()).popBackStack();
-                    }
-
-                    @Override
-                    public void onInvalid() {
-                        Snackbar.make(layout, R.string.invalid_password, Snackbar.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onFailed(String message) {
-                        Snackbar.make(layout, message, Snackbar.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onError() {
-                        Snackbar.make(layout, R.string.failed_update_password, Snackbar.LENGTH_LONG).show();
-                    }
-                });
+                buttonSave.setEnabled(false);
+                buttonSave.setText(R.string.loading);
+                updatePassword();
             }
         });
 
         return view;
+    }
+
+    private void updatePassword() {
+        // Menyimpan data
+        String currentPassword = Objects.requireNonNull(inputCurrentPassword.getText()).toString();
+        String newPassword = Objects.requireNonNull(inputNewPassword.getText()).toString();
+        String confirmPassword = Objects.requireNonNull(inputConfirmPassword.getText()).toString();
+
+        // Membuat objek
+        UpdatePasswordRequest updatePasswordRequest = new UpdatePasswordRequest(
+                context,
+                currentPassword,
+                newPassword,
+                confirmPassword
+        );
+
+        // Mengirim request
+        updatePasswordRequest.sendRequest(new UpdatePasswordRequest.APICallback() {
+            @Override
+            public void onSuccess() {
+                if (getFragmentManager() != null) {
+                    getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                }
+            }
+
+            @Override
+            public void onInvalid() {
+                buttonSave.setEnabled(true);
+                buttonSave.setText(R.string.save_password);
+                Snackbar.make(layout, R.string.invalid_current_password, Snackbar.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailed(String message) {
+                buttonSave.setEnabled(true);
+                buttonSave.setText(R.string.save_password);
+                Snackbar.make(layout, message, Snackbar.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError() {
+                buttonSave.setEnabled(true);
+                buttonSave.setText(R.string.save_password);
+                Snackbar.make(layout, R.string.failed_update_password, Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
