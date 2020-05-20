e258f58bc0986ba68ec465d88901da5abee80bec
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-20 10:47:17 +0700

    Refaktor model film self

--- app/src/main/java/xyz/fairportstudios/popularin/models/FilmSelf.java
+++ app/src/main/java/xyz/fairportstudios/popularin/models/FilmSelf.java
@@ -10,7 +10,12 @@ public class FilmSelf {
         // Empty constructor
     }
 
-    public FilmSelf(Boolean in_favorite, Boolean in_review, Boolean in_watchlist, Double last_rate) {
+    public FilmSelf(
+            Boolean in_favorite,
+            Boolean in_review,
+            Boolean in_watchlist,
+            Double last_rate
+    ) {
         this.in_favorite = in_favorite;
         this.in_review = in_review;
         this.in_watchlist = in_watchlist;
@@ -21,28 +26,28 @@ public class FilmSelf {
         return in_favorite;
     }
 
-    public Boolean getIn_review() {
-        return in_review;
-    }
-
-    public Boolean getIn_watchlist() {
-        return in_watchlist;
-    }
-
-    public Double getLast_rate() {
-        return last_rate;
-    }
-
     public void setIn_favorite(Boolean in_favorite) {
         this.in_favorite = in_favorite;
+    }
+
+    public Boolean getIn_review() {
+        return in_review;
     }
 
     public void setIn_review(Boolean in_review) {
         this.in_review = in_review;
     }
 
+    public Boolean getIn_watchlist() {
+        return in_watchlist;
+    }
+
     public void setIn_watchlist(Boolean in_watchlist) {
         this.in_watchlist = in_watchlist;
+    }
+
+    public Double getLast_rate() {
+        return last_rate;
     }
 
     public void setLast_rate(Double last_rate) {
