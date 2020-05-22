3ecb12c22c59c84dec55cdd0cdc24811aca76572
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-22 13:35:30 +0700

    Refaktor layout & logic fragment timeline

---
+++ app/src/main/java/xyz/fairportstudios/popularin/apis/popularin/get/TimelineRequest.java
@@ -0,0 +1,115 @@
+package xyz.fairportstudios.popularin.apis.popularin.get;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import xyz.fairportstudios.popularin.adapters.ReviewAdapter;
+import xyz.fairportstudios.popularin.apis.popularin.PopularinAPI;
+import xyz.fairportstudios.popularin.models.Review;
+import xyz.fairportstudios.popularin.preferences.Auth;
+
+public class TimelineRequest {
+    private Context context;
+    private List<Review> reviewList;
+    private RecyclerView recyclerView;
+
+    public TimelineRequest(Context context, List<Review> reviewList, RecyclerView recyclerView) {
+        this.context = context;
+        this.reviewList = reviewList;
+        this.recyclerView = recyclerView;
+    }
+
+    public interface APICallback {
+        void onSuccess();
+
+        void onEmpty();
+
+        void onError();
+    }
+
+    public void sendRequest(final APICallback callback) {
+        final Auth auth = new Auth(context);
+
+        String requestURL = PopularinAPI.TIMELINE + 1;
+
+        JsonObjectRequest timeline = new JsonObjectRequest(Request.Method.GET, requestURL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    int status = response.getInt("status");
+
+                    if (status == 101) {
+                        JSONObject resultObject = response.getJSONObject("result");
+                        JSONArray dataArray = resultObject.getJSONArray("data");
+
+                        for (int index = 0; index < dataArray.length(); index++) {
+                            JSONObject reviewObject = dataArray.getJSONObject(index);
+                            JSONObject filmObject = reviewObject.getJSONObject("film");
+                            JSONObject userObject = reviewObject.getJSONObject("user");
+
+                            Review review = new Review();
+                            review.setReview_id(reviewObject.getInt("id"));
+                            review.setTmdb_id(filmObject.getInt("tmdb_id"));
+                            review.setUser_id(userObject.getInt("id"));
+                            review.setRating(reviewObject.getDouble("rating"));
+                            review.setReview_detail(reviewObject.getString("review_detail"));
+                            review.setTimestamp(reviewObject.getString("timestamp"));
+                            review.setTitle(filmObject.getString("title"));
+                            review.setRelease_date(filmObject.getString("release_date"));
+                            review.setPoster(filmObject.getString("poster"));
+                            review.setUsername(userObject.getString("username"));
+                            review.setProfile_picture(userObject.getString("profile_picture"));
+                            reviewList.add(review);
+                        }
+
+                        ReviewAdapter reviewAdapter = new ReviewAdapter(context, reviewList);
+                        recyclerView.setAdapter(reviewAdapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                        recyclerView.setVisibility(View.VISIBLE);
+                        callback.onSuccess();
+                    } else if (status == 606) {
+                        callback.onEmpty();
+                    } else {
+                        callback.onError();
+                    }
+                } catch (JSONException exception) {
+                    exception.printStackTrace();
+                    callback.onError();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                callback.onError();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Auth-ID", auth.getAuthID());
+                headers.put("Auth-Token", auth.getAuthToken());
+                return headers;
+            }
+        };
+
+        Volley.newRequestQueue(context).add(timeline);
+    }
+}
--- app/src/main/java/xyz/fairportstudios/popularin/fragments/TimelineFragment.java
+++ app/src/main/java/xyz/fairportstudios/popularin/fragments/TimelineFragment.java
@@ -1,23 +1,76 @@
 package xyz.fairportstudios.popularin.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import xyz.fairportstudios.popularin.R;
+import xyz.fairportstudios.popularin.apis.popularin.get.TimelineRequest;
+import xyz.fairportstudios.popularin.models.Review;
 
 public class TimelineFragment extends Fragment {
+    private Context context;
+    private CoordinatorLayout anchorLayout;
+    private ProgressBar progressBar;
+    private RecyclerView recyclerTimeline;
+    private TextView textEmptyTimeline;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.reusable_recycler, container, false);
+
+        // Binding
+        context = getActivity();
+        anchorLayout = view.findViewById(R.id.anchor_rr_layout);
+        progressBar = view.findViewById(R.id.pbr_rr_layout);
+        recyclerTimeline = view.findViewById(R.id.recycler_rr_layout);
+        textEmptyTimeline = view.findViewById(R.id.text_rr_empty_result);
+
+        // Request
+        getTimeline();
 
         return view;
+    }
+
+    private void getTimeline() {
+        List<Review> reviewList = new ArrayList<>();
+        TimelineRequest timelineRequest = new TimelineRequest(context, reviewList, recyclerTimeline);
+        timelineRequest.sendRequest(new TimelineRequest.APICallback() {
+            @Override
+            public void onSuccess() {
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onEmpty() {
+                progressBar.setVisibility(View.GONE);
+                textEmptyTimeline.setVisibility(View.VISIBLE);
+                textEmptyTimeline.setText(R.string.empty_timeline);
+            }
+
+            @Override
+            public void onError() {
+                progressBar.setVisibility(View.GONE);
+                textEmptyTimeline.setVisibility(View.VISIBLE);
+                textEmptyTimeline.setText(R.string.empty_timeline);
+                Snackbar.make(anchorLayout, R.string.get_error, Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
