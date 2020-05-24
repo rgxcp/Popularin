66c2267221272a193d07fb20f514e29e29a6e06a
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-24 11:39:52 +0700

    Refaktor layout & logic activity film list

--- app/src/main/java/xyz/fairportstudios/popularin/activities/FilmListActivity.java
+++ app/src/main/java/xyz/fairportstudios/popularin/activities/FilmListActivity.java
@@ -7,6 +7,7 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,57 +20,78 @@ import java.util.List;
 import xyz.fairportstudios.popularin.R;
 import xyz.fairportstudios.popularin.apis.tmdb.get.DiscoverFilmRequest;
 import xyz.fairportstudios.popularin.models.Film;
+import xyz.fairportstudios.popularin.services.Popularin;
 
 public class FilmListActivity extends AppCompatActivity {
+    // Untuk fitur discover
+    private Integer currentPage = 1;
+
+    // Member variable
+    private List<Film> filmList;
     private ProgressBar progressBar;
-    private RelativeLayout layout;
-    private TextView emptyResult;
+    private RecyclerView recyclerFilm;
+    private RelativeLayout anchorLayout;
+    private String genreID;
+    private TextView textNetworkError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.global_toolbar_recycler);
+        setContentView(R.layout.reusable_toolbar_recycler);
 
         // Binding
-        progressBar = findViewById(R.id.pbr_gtr_layout);
-        layout = findViewById(R.id.layout_gtr_anchor);
-        emptyResult = findViewById(R.id.text_fp_empty);
-        RecyclerView recyclerView = findViewById(R.id.recycler_gtr_layout);
-        Toolbar toolbar = findViewById(R.id.toolbar_gtr_layout);
+        progressBar = findViewById(R.id.pbr_rtr_layout);
+        recyclerFilm = findViewById(R.id.recycler_rtr_layout);
+        anchorLayout = findViewById(R.id.anchor_rtr_layout);
+        textNetworkError = findViewById(R.id.text_rtr_network_error);
+        Toolbar toolbar = findViewById(R.id.toolbar_rtr_layout);
 
         // Extra
         Intent intent = getIntent();
-        String genreID = intent.getStringExtra("GENRE_ID");
-        String genreTitle = intent.getStringExtra("GENRE_TITLE");
+        genreID = intent.getStringExtra(Popularin.GENRE_ID);
+        String genreTitle = intent.getStringExtra(Popularin.GENRE_TITLE);
 
         // Toolbar
         toolbar.setTitle(genreTitle);
 
-        // List
-        List<Film> filmList = new ArrayList<>();
-
-        // GET
-        DiscoverFilmRequest discoverFilmRequest = new DiscoverFilmRequest(this, filmList, recyclerView);
-        String requestURL = discoverFilmRequest.getRequestURL(genreID, 1);
-        discoverFilmRequest.sendRequest(requestURL, new DiscoverFilmRequest.APICallback() {
-            @Override
-            public void onSuccess() {
-                progressBar.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onError() {
-                progressBar.setVisibility(View.GONE);
-                emptyResult.setVisibility(View.VISIBLE);
-                Snackbar.make(layout, R.string.network_error, Snackbar.LENGTH_LONG).show();
-            }
-        });
+        // Mendapatkan data
+        filmList = new ArrayList<>();
+        discoverFilm(currentPage);
 
         // Activity
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+
+        recyclerFilm.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (!recyclerView.canScrollVertically(1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    discoverFilm(currentPage);
+                }
+            }
+        });
+    }
+
+    private void discoverFilm(Integer page) {
+        DiscoverFilmRequest discoverFilmRequest = new DiscoverFilmRequest(this, genreID, filmList, recyclerFilm);
+        String requestURL = discoverFilmRequest.getRequestURL(page);
+        discoverFilmRequest.sendRequest(requestURL, new DiscoverFilmRequest.APICallback() {
+            @Override
+            public void onSuccess() {
+                progressBar.setVisibility(View.GONE);
+                currentPage++;
+            }
+
+            @Override
+            public void onError() {
+                progressBar.setVisibility(View.GONE);
+                textNetworkError.setVisibility(View.VISIBLE);
+                Snackbar.make(anchorLayout, R.string.network_error, Snackbar.LENGTH_LONG).show();
             }
         });
     }
