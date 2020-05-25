fb01661070f927de83c0a2884a0b8889c0467416
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-25 20:22:02 +0700

    Refaktor model user review

--- app/src/main/java/xyz/fairportstudios/popularin/models/UserReview.java
+++ app/src/main/java/xyz/fairportstudios/popularin/models/UserReview.java
@@ -4,88 +4,97 @@ public class UserReview {
     private Integer id;
     private Integer tmdb_id;
     private Double rating;
-    private String poster;
-    private String release_date;
-    private String review_date;
-    private String review_text;
+    private String review_detail;
+    private String timestamp;
     private String title;
+    private String release_date;
+    private String poster;
 
     public UserReview() {
-        // Empty constructor
+        // Constructor kosong
     }
 
-    public UserReview(Integer id, Integer tmdb_id, Double rating, String poster, String release_date, String review_date, String review_text, String title) {
+    public UserReview(
+            Integer id,
+            Integer tmdb_id,
+            Double rating,
+            String review_detail,
+            String timestamp,
+            String title,
+            String release_date,
+            String poster
+    ) {
         this.id = id;
         this.tmdb_id = tmdb_id;
         this.rating = rating;
-        this.poster = poster;
-        this.release_date = release_date;
-        this.review_date = review_date;
-        this.review_text = review_text;
+        this.review_detail = review_detail;
+        this.timestamp = timestamp;
         this.title = title;
+        this.release_date = release_date;
+        this.poster = poster;
     }
 
     public Integer getId() {
         return id;
     }
 
-    public Integer getTmdb_id() {
-        return tmdb_id;
-    }
-
-    public Double getRating() {
-        return rating;
-    }
-
-    public String getPoster() {
-        return poster;
-    }
-
-    public String getRelease_date() {
-        return release_date;
-    }
-
-    public String getReview_date() {
-        return review_date;
-    }
-
-    public String getReview_text() {
-        return review_text;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Integer getTmdb_id() {
+        return tmdb_id;
     }
 
     public void setTmdb_id(Integer tmdb_id) {
         this.tmdb_id = tmdb_id;
     }
 
+    public Double getRating() {
+        return rating;
+    }
+
     public void setRating(Double rating) {
         this.rating = rating;
     }
 
-    public void setPoster(String poster) {
-        this.poster = poster;
+    public String getReview_detail() {
+        return review_detail;
+    }
+
+    public void setReview_detail(String review_detail) {
+        this.review_detail = review_detail;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRelease_date() {
+        return release_date;
     }
 
     public void setRelease_date(String release_date) {
         this.release_date = release_date;
     }
 
-    public void setReview_date(String review_date) {
-        this.review_date = review_date;
+    public String getPoster() {
+        return poster;
     }
 
-    public void setReview_text(String review_text) {
-        this.review_text = review_text;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
+    public void setPoster(String poster) {
+        this.poster = poster;
     }
 }
