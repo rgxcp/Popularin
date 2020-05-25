067b1f45f5a31318d5a5cd93d86f83fa3e6791de
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-25 13:14:36 +0700

    Refaktor logic activity social

--- app/src/main/java/xyz/fairportstudios/popularin/activities/SocialActivity.java
+++ app/src/main/java/xyz/fairportstudios/popularin/activities/SocialActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
@@ -15,6 +16,8 @@ import xyz.fairportstudios.popularin.adapters.PagerAdapter;
 import xyz.fairportstudios.popularin.fragments.FollowerFragment;
 import xyz.fairportstudios.popularin.fragments.FollowingFragment;
 import xyz.fairportstudios.popularin.fragments.MutualFragment;
+import xyz.fairportstudios.popularin.preferences.Auth;
+import xyz.fairportstudios.popularin.services.Popularin;
 
 public class SocialActivity extends AppCompatActivity {
 
@@ -30,21 +33,21 @@ public class SocialActivity extends AppCompatActivity {
 
         // Extra
         Intent intent = getIntent();
-        String userID = intent.getStringExtra("USER_ID");
-        boolean isSelf = intent.getBooleanExtra("IS_SELF", false);
+        String userID = intent.getStringExtra(Popularin.USER_ID);
+        boolean isSelf = intent.getBooleanExtra(Popularin.IS_SELF, false);
+
+        // Auth
+        boolean isAuth = new Auth(this).isAuth();
 
         // Toolbar
-        toolbar.setTitle("Social");
+        toolbar.setTitle(R.string.social);
 
-        // Tab
-        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), 0);
-        if (isSelf) {
-            pagerAdapter.addFragment(new FollowingFragment(userID), "MENGIKUTI");
-            pagerAdapter.addFragment(new FollowerFragment(userID), "PENGIKUT");
-        } else {
-            pagerAdapter.addFragment(new MutualFragment(userID), "MUTUAL");
-            pagerAdapter.addFragment(new FollowingFragment(userID), "MENGIKUTI");
-            pagerAdapter.addFragment(new FollowerFragment(userID), "PENGIKUT");
+        // Tab Pager
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        pagerAdapter.addFragment(new FollowingFragment(userID), getString(R.string.following));
+        pagerAdapter.addFragment(new FollowerFragment(userID), getString(R.string.follower));
+        if (isAuth && !isSelf) {
+            pagerAdapter.addFragment(new MutualFragment(userID), getString(R.string.mutual));
         }
         viewPager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
