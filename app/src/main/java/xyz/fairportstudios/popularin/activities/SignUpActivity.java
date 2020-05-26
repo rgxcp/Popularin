5d59bfd5531641aa2600554faf9762b75845a2c1
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-26 17:34:30 +0700

    Refaktor layout & logic activity sign up

--- app/src/main/java/xyz/fairportstudios/popularin/activities/SignUpActivity.java
+++ app/src/main/java/xyz/fairportstudios/popularin/activities/SignUpActivity.java
@@ -1,13 +1,19 @@
 package xyz.fairportstudios.popularin.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.SpannableString;
+import android.text.TextWatcher;
+import android.text.style.RelativeSizeSpan;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
@@ -21,9 +27,12 @@ import xyz.fairportstudios.popularin.preferences.Auth;
 public class SignUpActivity extends AppCompatActivity {
     private Button buttonSignUp;
     private Context context;
-    private LinearLayout layout;
-    private TextInputEditText inputFirstName;
-    private TextInputEditText inputLastName;
+    private LinearLayout anchorLayout;
+    private String fullName;
+    private String username;
+    private String email;
+    private String password;
+    private TextInputEditText inputFullName;
     private TextInputEditText inputUsername;
     private TextInputEditText inputEmail;
     private TextInputEditText inputPassword;
@@ -36,68 +45,132 @@ public class SignUpActivity extends AppCompatActivity {
         // Binding
         context = SignUpActivity.this;
         buttonSignUp = findViewById(R.id.button_asu_sign_up);
-        layout = findViewById(R.id.layout_asu_anchor);
-        inputFirstName = findViewById(R.id.input_asu_first_name);
-        inputLastName = findViewById(R.id.input_asu_last_name);
+        anchorLayout = findViewById(R.id.anchor_asu_layout);
+        inputFullName = findViewById(R.id.input_asu_full_name);
         inputUsername = findViewById(R.id.input_asu_username);
         inputEmail = findViewById(R.id.input_asu_email);
         inputPassword = findViewById(R.id.input_asu_password);
+        TextView textWelcome = findViewById(R.id.text_asu_welcome);
+
+        // Pesan
+        String welcome = getString(R.string.sign_up_welcome);
+        SpannableString spannableString = new SpannableString(welcome);
+        RelativeSizeSpan relativeSizeSpan = new RelativeSizeSpan(2f);
+        spannableString.setSpan(relativeSizeSpan, 0, 5, 0);
+        textWelcome.setText(spannableString);
+
+        // Text watcher
+        inputFullName.addTextChangedListener(signUpWatcher);
+        inputUsername.addTextChangedListener(signUpWatcher);
+        inputEmail.addTextChangedListener(signUpWatcher);
+        inputPassword.addTextChangedListener(signUpWatcher);
 
         // Activity
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                buttonSignUp.setEnabled(false);
-                buttonSignUp.setText(R.string.loading);
                 signUp();
             }
         });
     }
 
+    private TextWatcher signUpWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            // Tidak digunakan
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            fullName = Objects.requireNonNull(inputFullName.getText()).toString();
+            username = Objects.requireNonNull(inputUsername.getText()).toString();
+            email = Objects.requireNonNull(inputEmail.getText()).toString();
+            password = Objects.requireNonNull(inputPassword.getText()).toString();
+            buttonSignUp.setEnabled(!fullName.isEmpty() && !username.isEmpty() && !email.isEmpty() && !password.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            // Tidak digunakan
+        }
+    };
+
+    private boolean validateFullName() {
+        if (!fullName.contains(" ")) {
+            Snackbar.make(anchorLayout, R.string.validate_full_name, Snackbar.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validateUsername() {
+        if (username.length() < 5) {
+            Snackbar.make(anchorLayout, R.string.validate_username, Snackbar.LENGTH_LONG).show();
+            return false;
+        } else if (username.contains(" ")) {
+            Snackbar.make(anchorLayout, R.string.validate_alpha_dash, Snackbar.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validateEmail() {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Snackbar.make(anchorLayout, R.string.validate_email, Snackbar.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        if (password.length() < 8) {
+            Snackbar.make(anchorLayout, R.string.validate_password, Snackbar.LENGTH_LONG).show();
+            return false;
+        } else if (password.equals(username)) {
+            Snackbar.make(anchorLayout, R.string.validate_match_password, Snackbar.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     private void signUp() {
-        // Menyimpan input
-        String firstName = Objects.requireNonNull(inputFirstName.getText()).toString();
-        String lastName = Objects.requireNonNull(inputLastName.getText()).toString();
-        String username = Objects.requireNonNull(inputUsername.getText()).toString();
-        String email = Objects.requireNonNull(inputEmail.getText()).toString();
-        String password = Objects.requireNonNull(inputPassword.getText()).toString();
+        buttonSignUp.setEnabled(false);
+        buttonSignUp.setText(R.string.loading);
 
-        // Membuat objek
-        SignUpRequest signUpRequest = new SignUpRequest(
-                context,
-                firstName,
-                lastName,
-                username,
-                email,
-                password
-        );
+        if (validateFullName() && validateUsername() && validateEmail() && validatePassword()) {
+            SignUpRequest signUpRequest = new SignUpRequest(context, fullName, username, email, password);
+            signUpRequest.sendRequest(new SignUpRequest.APICallback() {
+                @Override
+                public void onSuccess(String id, String token) {
+                    Auth auth = new Auth(context);
+                    auth.setAuth(id, token);
 
-        // Mengirim request
-        signUpRequest.sendRequest(new SignUpRequest.APICallback() {
-            @Override
-            public void onSuccess(String id, String token) {
-                // Menyimpan pref dalam storage lokal
-                Auth auth = new Auth(context);
-                auth.setAuth(id, token);
+                    Intent intent = new Intent(context, MainActivity.class);
+                    startActivity(intent);
+                    finishAffinity();
+                }
 
-                Intent intent = new Intent(context, MainActivity.class);
-                startActivity(intent);
-                finishAffinity();
-            }
+                @Override
+                public void onFailed(String message) {
+                    buttonSignUp.setEnabled(true);
+                    buttonSignUp.setText(R.string.sign_up);
+                    Snackbar.make(anchorLayout, message, Snackbar.LENGTH_LONG).show();
+                }
 
-            @Override
-            public void onFailed(String message) {
-                buttonSignUp.setEnabled(true);
-                buttonSignUp.setText(R.string.sign_up);
-                Snackbar.make(layout, message, Snackbar.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onError() {
-                buttonSignUp.setEnabled(true);
-                buttonSignUp.setText(R.string.sign_up);
-                Snackbar.make(layout, R.string.sign_up_error, Snackbar.LENGTH_LONG).show();
-            }
-        });
+                @Override
+                public void onError() {
+                    buttonSignUp.setEnabled(true);
+                    buttonSignUp.setText(R.string.sign_up);
+                    Snackbar.make(anchorLayout, R.string.failed_sign_up, Snackbar.LENGTH_LONG).show();
+                }
+            });
+        } else {
+            buttonSignUp.setEnabled(true);
+            buttonSignUp.setText(R.string.sign_up);
+        }
     }
 }
