e3d24f97f9df086f16e4a1ae59a15229f1d9ad80
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-27 05:36:27 +0700

    Refaktor model self detail

--- app/src/main/java/xyz/fairportstudios/popularin/models/SelfDetail.java
+++ app/src/main/java/xyz/fairportstudios/popularin/models/SelfDetail.java
@@ -1,41 +1,30 @@
 package xyz.fairportstudios.popularin.models;
 
 public class SelfDetail {
-    private String first_name;
-    private String last_name;
+    private String full_name;
     private String username;
     private String email;
 
     public SelfDetail() {
-        // Empty constructor
+        // Constructor kosong
     }
 
     public SelfDetail(
-            String first_name,
-            String last_name,
+            String full_name,
             String username,
             String email
     ) {
-        this.first_name = first_name;
-        this.last_name = last_name;
+        this.full_name = full_name;
         this.username = username;
         this.email = email;
     }
 
-    public String getFirst_name() {
-        return first_name;
+    public String getFull_name() {
+        return full_name;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
     }
 
     public String getUsername() {
