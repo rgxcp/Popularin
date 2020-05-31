bed78e1a67f8372af5091c3bb72b7c50084f9720
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-31 08:49:05 +0700

    Refaktor class api Popularin & TMDb

--- app/src/main/java/xyz/fairportstudios/popularin/apis/popularin/PopularinAPI.java
+++ app/src/main/java/xyz/fairportstudios/popularin/apis/popularin/PopularinAPI.java
@@ -1,21 +1,16 @@
 package xyz.fairportstudios.popularin.apis.popularin;
 
 public class PopularinAPI {
-    // Safe
     private static final String BASE = "https://popularin.fairportstudios.xyz/api";
-    public static final String COMMENT = PopularinAPI.BASE + "/comment";
-    public static final String FILM = PopularinAPI.BASE + "/film";
-    public static final String REVIEW = PopularinAPI.BASE + "/review";
-    public static final String TIMELINE = PopularinAPI.BASE + "/review/timeline?page=";
-
-    // Unsafe
-    public static final String FAVORITE = "https://popularin.fairportstudios.xyz/api/favorite";
-    public static final String REVIEWS = "https://popularin.fairportstudios.xyz/api/reviews";
-    public static final String SEARCH_USER = "https://popularin.fairportstudios.xyz/api/user/search?query=";
-    public static final String SIGN_IN = "https://popularin.fairportstudios.xyz/api/user/signin";
-    public static final String SIGN_UP = "https://popularin.fairportstudios.xyz/api/user/signup";
-    public static final String SIGN_OUT = "https://popularin.fairportstudios.xyz/api/user/signout";
-    public static final String USER = "https://popularin.fairportstudios.xyz/api/user";
-    public static final String USER_SELF = "https://popularin.fairportstudios.xyz/api/user/self";
-    public static final String WATCHLIST = "https://popularin.fairportstudios.xyz/api/watchlist";
+    public static final String COMMENT = BASE + "/comment";
+    public static final String FILM = BASE + "/film";
+    public static final String REVIEW = BASE + "/review";
+    public static final String REVIEWS = BASE + "/reviews";
+    public static final String SEARCH_USER = BASE + "/user/search";
+    public static final String SIGN_IN = BASE + "/user/signin";
+    public static final String SIGN_OUT = BASE + "/user/signout";
+    public static final String SIGN_UP = BASE + "/user/signup";
+    public static final String TIMELINE = BASE + "/review/timeline?page=";
+    public static final String USER = BASE + "/user";
+    public static final String USER_SELF = BASE + "/user/self";
 }
--- app/src/main/java/xyz/fairportstudios/popularin/apis/tmdb/TMDbAPI.java
+++ app/src/main/java/xyz/fairportstudios/popularin/apis/tmdb/TMDbAPI.java
@@ -1,12 +1,12 @@
 package xyz.fairportstudios.popularin.apis.tmdb;
 
 public class TMDbAPI {
-    public static final String API_KEY = "0cdb7eb7a8102f652a6c74dddd692a2f";
-    public static final String BASE = "https://api.themoviedb.org/3";
+    private static final String BASE = "https://api.themoviedb.org/3";
     public static final String IMAGE = "https://image.tmdb.org/t/p/w300";
-    public static final String AIRING_FILM = "https://api.themoviedb.org/3/movie/now_playing";
-    public static final String CREDIT = "https://api.themoviedb.org/3/person/";
-    public static final String DISCOVER_GENRE = "https://api.themoviedb.org/3/discover/movie";
-    public static final String FILM_DETAIL = "https://api.themoviedb.org/3/movie/";
-    public static final String SEARCH_FILM = "https://api.themoviedb.org/3/search/movie";
+    public static final String API_KEY = "0cdb7eb7a8102f652a6c74dddd692a2f";
+    public static final String AIRING_FILM = BASE + "/movie/now_playing";
+    public static final String CREDIT = BASE + "/person/";
+    public static final String DISCOVER_FILM = BASE + "/discover/movie";
+    public static final String FILM_DETAIL = BASE + "/movie/";
+    public static final String SEARCH_FILM = BASE + "/search/movie?api_key=" + API_KEY + "&language=id&query=";
 }
