73363f77a62162e08a8197dffb35014b0ad37545
Rommy Gustiawan <rgcppp@gmail.com>
2020-05-31 08:48:16 +0700

    Membuat class static untuk extra intent & tag fragment

---
+++ app/src/main/java/xyz/fairportstudios/popularin/services/Popularin.java
@@ -0,0 +1,17 @@
+package xyz.fairportstudios.popularin.services;
+
+public class Popularin {
+    public static final String DATE_PICKER = "DATE_PCIKER";
+    public static final String FILM_ID = "FILM_ID";
+    public static final String FILM_POSTER = "FILM_POSTER";
+    public static final String FILM_STATUS_MODAL = "FILM_STATUS_MODAL";
+    public static final String FILM_TITLE = "FILM_TITLE";
+    public static final String FILM_YEAR = "FILM_YEAR";
+    public static final String GENRE_ID = "GENRE_ID";
+    public static final String GENRE_TITLE = "GENRE_TITLE";
+    public static final String IS_SELF = "IS_SELF";
+    public static final String MAIN_BACK_STACK = "MAIN_BACK_STACK";
+    public static final String RATING = "RATING";
+    public static final String REVIEW_ID = "REVIEW_ID";
+    public static final String USER_ID = "USER_ID";
+}
