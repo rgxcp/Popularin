568e69df4281bd2267c1f10a02d03eb2ae73aa8c
Rommy Gustiawan <rgcppp@gmail.com>
2020-06-04 19:55:35 +0700

    Menghapus class yang tidak digunakan

--- app/src/main/java/xyz/fairportstudios/popularin/apis/popularin/delete/DeleteUser.java
+++
@@ -1,9 +0,0 @@
-package xyz.fairportstudios.popularin.apis.popularin.delete;
-
-import xyz.fairportstudios.popularin.apis.popularin.PopularinAPI;
-
-public class DeleteUser {
-    private String id;
-
-    String requestURL = PopularinAPI.USER + "/" + id;
-}
--- app/src/main/java/xyz/fairportstudios/popularin/apis/popularin/put/UpdateFilmOverview.java
+++
@@ -1,9 +0,0 @@
-package xyz.fairportstudios.popularin.apis.popularin.put;
-
-import xyz.fairportstudios.popularin.apis.popularin.PopularinAPI;
-
-public class UpdateFilmOverview {
-    private String id;
-
-    String requestURL = PopularinAPI.FILM + "/" + id;
-}
