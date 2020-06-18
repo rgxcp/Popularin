2cf068b03a2a85599b242d0b647afa539d608cf9
Rommy Gustiawan <rgcppp@gmail.com>
2020-06-18 09:12:44 +0700

    Memindahkan api key ke dalam class sendiri

---
+++ app/src/main/java/xyz/fairportstudios/popularin/secrets/APIKey.java
@@ -0,0 +1,6 @@
+package xyz.fairportstudios.popularin.secrets;
+
+public class APIKey {
+    public static final String POPULARIN_API_KEY = "39ede2bffc5f3da5de35ff21dc980ad938888e914f6e04e79b03d4de4374cde9";
+    public static final String TMDB_API_KEY = "0cdb7eb7a8102f652a6c74dddd692a2f";
+}
