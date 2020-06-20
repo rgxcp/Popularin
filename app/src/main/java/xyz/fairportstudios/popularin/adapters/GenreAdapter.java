dc42b7b7ada07635a997672c592f6a6733d72f36
Rommy Gustiawan <rgcppp@gmail.com>
2020-06-20 11:05:02 +0700

    Mengimplementasi on click listener dalam adapter genre

--- app/src/main/java/xyz/fairportstudios/popularin/adapters/GenreAdapter.java
+++ app/src/main/java/xyz/fairportstudios/popularin/adapters/GenreAdapter.java
@@ -1,7 +1,6 @@
 package xyz.fairportstudios.popularin.adapters;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,46 +13,48 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 import xyz.fairportstudios.popularin.R;
-import xyz.fairportstudios.popularin.activities.FilmListActivity;
 import xyz.fairportstudios.popularin.models.Genre;
-import xyz.fairportstudios.popularin.services.ConvertPixel;
-import xyz.fairportstudios.popularin.statics.Popularin;
 
 public class GenreAdapter extends RecyclerView.Adapter<GenreAdapter.GenreViewHolder> {
-    private Context context;
-    private List<Genre> genreList;
+    private Context mContext;
+    private List<Genre> mGenreList;
+    private OnClickListener mOnClickListener;
 
-    public GenreAdapter(Context context, List<Genre> genreList) {
-        this.context = context;
-        this.genreList = genreList;
+    public GenreAdapter(Context context, List<Genre> genreList, OnClickListener onClickListener) {
+        mContext = context;
+        mGenreList = genreList;
+        mOnClickListener = onClickListener;
+    }
+
+    public interface OnClickListener {
+        void onItemClick(int position);
+    }
+
+    public int getDensity(int px) {
+        float dp = px * mContext.getResources().getDisplayMetrics().density;
+        return (int) dp;
     }
 
     @NonNull
     @Override
     public GenreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new GenreViewHolder(LayoutInflater.from(context).inflate(R.layout.recycler_genre, parent, false));
+        return new GenreViewHolder(LayoutInflater.from(mContext).inflate(R.layout.recycler_genre, parent, false), mOnClickListener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull GenreViewHolder holder, int position) {
         // Posisi
-        Genre currentItem = genreList.get(position);
-
-        // Extra
-        final Integer genreID = currentItem.getId();
-        final String genreTitle = currentItem.getTitle();
+        Genre currentItem = mGenreList.get(position);
 
         // Isi
-        holder.textGenreTitle.setText(genreTitle);
-        holder.imageGenreBackground.setImageResource(currentItem.getBackground());
+        holder.mTextTitle.setText(currentItem.getTitle());
+        holder.mImageBackground.setImageResource(currentItem.getBackground());
 
         // Margin
-        ConvertPixel convertPixel = new ConvertPixel(context);
-
-        int left = convertPixel.getDensity(4);
-        int top = convertPixel.getDensity(4);
-        int right = convertPixel.getDensity(4);
-        int bottom = convertPixel.getDensity(4);
+        int left = getDensity(4);
+        int top = getDensity(4);
+        int right = getDensity(4);
+        int bottom = getDensity(4);
 
         boolean isEdgeLeft = (position % 2) == 0;
         boolean isEdgeTop = position < 2;
@@ -61,48 +62,48 @@ public class GenreAdapter extends RecyclerView.Adapter<GenreAdapter.GenreViewHol
         boolean isEdgeBottom = position >= (getItemCount() - 2);
 
         if (isEdgeLeft) {
-            left = convertPixel.getDensity(16);
+            left = getDensity(16);
         }
         if (isEdgeTop) {
-            top = convertPixel.getDensity(16);
+            top = getDensity(16);
         }
         if (isEdgeRight) {
-            right = convertPixel.getDensity(16);
+            right = getDensity(16);
         }
         if (isEdgeBottom) {
-            bottom = convertPixel.getDensity(16);
+            bottom = getDensity(16);
         }
 
         ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) holder.itemView.getLayoutParams();
         layoutParams.setMargins(left, top, right, bottom);
         holder.itemView.setLayoutParams(layoutParams);
-
-        // Activity
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, FilmListActivity.class);
-                intent.putExtra(Popularin.GENRE_ID, genreID);
-                intent.putExtra(Popularin.GENRE_TITLE, genreTitle);
-                context.startActivity(intent);
-            }
-        });
     }
 
     @Override
     public int getItemCount() {
-        return genreList.size();
+        return mGenreList.size();
     }
 
-    static class GenreViewHolder extends RecyclerView.ViewHolder {
-        private TextView textGenreTitle;
-        private ImageView imageGenreBackground;
+    static class GenreViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private ImageView mImageBackground;
+        private TextView mTextTitle;
+        private OnClickListener mOnClickListener;
 
-        GenreViewHolder(@NonNull View itemView) {
+        GenreViewHolder(@NonNull View itemView, OnClickListener onClickListener) {
             super(itemView);
 
-            textGenreTitle = itemView.findViewById(R.id.text_rg_title);
-            imageGenreBackground = itemView.findViewById(R.id.image_rg_background);
+            mImageBackground = itemView.findViewById(R.id.image_rg_background);
+            mTextTitle = itemView.findViewById(R.id.text_rg_title);
+            mOnClickListener = onClickListener;
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v == itemView) {
+                mOnClickListener.onItemClick(getAdapterPosition());
+            }
         }
     }
 }
--- app/src/main/java/xyz/fairportstudios/popularin/fragments/GenreFragment.java
+++ app/src/main/java/xyz/fairportstudios/popularin/fragments/GenreFragment.java
@@ -1,6 +1,7 @@
 package xyz.fairportstudios.popularin.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,13 +19,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import xyz.fairportstudios.popularin.R;
+import xyz.fairportstudios.popularin.activities.FilmListActivity;
 import xyz.fairportstudios.popularin.adapters.GenreAdapter;
 import xyz.fairportstudios.popularin.models.Genre;
+import xyz.fairportstudios.popularin.statics.Popularin;
 import xyz.fairportstudios.popularin.statics.TMDbAPI;
 
-public class GenreFragment extends Fragment {
-    private ProgressBar progressBar;
-    private RecyclerView recyclerGenre;
+public class GenreFragment extends Fragment implements GenreAdapter.OnClickListener {
+    private Context mContext;
+    private GenreAdapter.OnClickListener mOnClickListener;
+    private List<Genre> mGenreList;
+    private ProgressBar mProgressBar;
+    private RecyclerView mRecyclerGenre;
 
     @Nullable
     @Override
@@ -32,15 +38,17 @@ public class GenreFragment extends Fragment {
         View view = inflater.inflate(R.layout.reusable_recycler, container, false);
 
         // Context
-        Context context = getActivity();
+        mContext = getActivity();
 
         // Binding
-        progressBar = view.findViewById(R.id.pbr_rr_layout);
-        recyclerGenre = view.findViewById(R.id.recycler_rr_layout);
+        mProgressBar = view.findViewById(R.id.pbr_rr_layout);
+        mRecyclerGenre = view.findViewById(R.id.recycler_rr_layout);
         final SwipeRefreshLayout swipeRefresh = view.findViewById(R.id.swipe_refresh_rr_layout);
 
-        // Menampilkan semua genre
-        showAllGenre(context);
+        // Menampilkan genre
+        mOnClickListener = this;
+        mGenreList = new ArrayList<>();
+        showGenre();
 
         // Activity
         swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -53,30 +61,42 @@ public class GenreFragment extends Fragment {
         return view;
     }
 
-    private void showAllGenre(Context context) {
-        List<Genre> genreList = new ArrayList<>();
-        genreList.add(new Genre(TMDbAPI.ACTION_ID, R.drawable.img_action, getString(R.string.genre_action)));
-        genreList.add(new Genre(TMDbAPI.ANIMATION_ID, R.drawable.img_animation, getString(R.string.genre_animation)));
-        genreList.add(new Genre(TMDbAPI.DOCUMENTARY_ID, R.drawable.img_documentary, getString(R.string.genre_documentary)));
-        genreList.add(new Genre(TMDbAPI.DRAMA_ID, R.drawable.img_drama, getString(R.string.genre_drama)));
-        genreList.add(new Genre(TMDbAPI.FANTASY_ID, R.drawable.img_fantasy, getString(R.string.genre_fantasy)));
-        genreList.add(new Genre(TMDbAPI.FICTION_ID, R.drawable.img_fiction, getString(R.string.genre_fiction)));
-        genreList.add(new Genre(TMDbAPI.HORROR_ID, R.drawable.img_horror, getString(R.string.genre_horror)));
-        genreList.add(new Genre(TMDbAPI.CRIME_ID, R.drawable.img_crime, getString(R.string.genre_crime)));
-        genreList.add(new Genre(TMDbAPI.FAMILY_ID, R.drawable.img_family, getString(R.string.genre_family)));
-        genreList.add(new Genre(TMDbAPI.COMEDY_ID, R.drawable.img_comedy, getString(R.string.genre_comedy)));
-        genreList.add(new Genre(TMDbAPI.MYSTERY_ID, R.drawable.img_mystery, getString(R.string.genre_mystery)));
-        genreList.add(new Genre(TMDbAPI.WAR_ID, R.drawable.img_war, getString(R.string.genre_war)));
-        genreList.add(new Genre(TMDbAPI.ADVENTURE_ID, R.drawable.img_adventure, getString(R.string.genre_adventure)));
-        genreList.add(new Genre(TMDbAPI.ROMANCE_ID, R.drawable.img_romance, getString(R.string.genre_romance)));
-        genreList.add(new Genre(TMDbAPI.HISTORY_ID, R.drawable.img_history, getString(R.string.genre_history)));
-        genreList.add(new Genre(TMDbAPI.THRILLER_ID, R.drawable.img_thriller, getString(R.string.genre_thriller)));
+    @Override
+    public void onItemClick(int position) {
+        Genre currentItem = mGenreList.get(position);
+        gotoFilmList(currentItem.getId(), currentItem.getTitle());
+    }
 
-        GenreAdapter genreAdapter = new GenreAdapter(context, genreList);
-        recyclerGenre.setAdapter(genreAdapter);
-        recyclerGenre.setLayoutManager(new GridLayoutManager(context, 2));
-        recyclerGenre.setHasFixedSize(true);
-        recyclerGenre.setVisibility(View.VISIBLE);
-        progressBar.setVisibility(View.GONE);
+    private void showGenre() {
+        mGenreList.add(new Genre(TMDbAPI.ACTION_ID, R.drawable.img_action, getString(R.string.genre_action)));
+        mGenreList.add(new Genre(TMDbAPI.ANIMATION_ID, R.drawable.img_animation, getString(R.string.genre_animation)));
+        mGenreList.add(new Genre(TMDbAPI.DOCUMENTARY_ID, R.drawable.img_documentary, getString(R.string.genre_documentary)));
+        mGenreList.add(new Genre(TMDbAPI.DRAMA_ID, R.drawable.img_drama, getString(R.string.genre_drama)));
+        mGenreList.add(new Genre(TMDbAPI.FANTASY_ID, R.drawable.img_fantasy, getString(R.string.genre_fantasy)));
+        mGenreList.add(new Genre(TMDbAPI.FICTION_ID, R.drawable.img_fiction, getString(R.string.genre_fiction)));
+        mGenreList.add(new Genre(TMDbAPI.HORROR_ID, R.drawable.img_horror, getString(R.string.genre_horror)));
+        mGenreList.add(new Genre(TMDbAPI.CRIME_ID, R.drawable.img_crime, getString(R.string.genre_crime)));
+        mGenreList.add(new Genre(TMDbAPI.FAMILY_ID, R.drawable.img_family, getString(R.string.genre_family)));
+        mGenreList.add(new Genre(TMDbAPI.COMEDY_ID, R.drawable.img_comedy, getString(R.string.genre_comedy)));
+        mGenreList.add(new Genre(TMDbAPI.MYSTERY_ID, R.drawable.img_mystery, getString(R.string.genre_mystery)));
+        mGenreList.add(new Genre(TMDbAPI.WAR_ID, R.drawable.img_war, getString(R.string.genre_war)));
+        mGenreList.add(new Genre(TMDbAPI.ADVENTURE_ID, R.drawable.img_adventure, getString(R.string.genre_adventure)));
+        mGenreList.add(new Genre(TMDbAPI.ROMANCE_ID, R.drawable.img_romance, getString(R.string.genre_romance)));
+        mGenreList.add(new Genre(TMDbAPI.HISTORY_ID, R.drawable.img_history, getString(R.string.genre_history)));
+        mGenreList.add(new Genre(TMDbAPI.THRILLER_ID, R.drawable.img_thriller, getString(R.string.genre_thriller)));
+
+        GenreAdapter genreAdapter = new GenreAdapter(mContext, mGenreList, mOnClickListener);
+        mRecyclerGenre.setAdapter(genreAdapter);
+        mRecyclerGenre.setLayoutManager(new GridLayoutManager(mContext, 2));
+        mRecyclerGenre.setHasFixedSize(true);
+        mRecyclerGenre.setVisibility(View.VISIBLE);
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+    private void gotoFilmList(int id, String title) {
+        Intent intent = new Intent(mContext, FilmListActivity.class);
+        intent.putExtra(Popularin.GENRE_ID, id);
+        intent.putExtra(Popularin.GENRE_TITLE, title);
+        startActivity(intent);
     }
 }
