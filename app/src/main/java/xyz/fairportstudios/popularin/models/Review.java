bde724278d0c2f5328bd0b65b3f29812a1545920
Rommy Gustiawan <rgcppp@gmail.com>
2020-06-21 11:27:03 +0700

    Mengubah tipe data menjadi primitive & menghapus setter yang tidak digunakan dalam model review

--- app/src/main/java/xyz/fairportstudios/popularin/models/Review.java
+++ app/src/main/java/xyz/fairportstudios/popularin/models/Review.java
@@ -1,33 +1,29 @@
 package xyz.fairportstudios.popularin.models;
 
 public class Review {
-    private Integer id;
-    private Integer tmdb_id;
-    private Integer user_id;
-    private Integer total_like;
-    private Integer total_comment;
-    private Boolean is_liked;
-    private Double rating;
-    private String review_detail;
-    private String timestamp;
-    private String title;
-    private String release_date;
-    private String poster;
-    private String username;
-    private String profile_picture;
-
-    public Review() {
-        // Constructor kosong
-    }
+    private int mId;
+    private int mTmdb_id;
+    private int mUser_id;
+    private int mTotal_like;
+    private int mTotal_comment;
+    private boolean mIs_liked;
+    private double mRating;
+    private String mReview_detail;
+    private String mTimestamp;
+    private String mTitle;
+    private String mRelease_date;
+    private String mPoster;
+    private String mUsername;
+    private String mProfile_picture;
 
     public Review(
-            Integer id,
-            Integer tmdb_id,
-            Integer user_id,
-            Integer total_like,
-            Integer total_comment,
-            Boolean is_liked,
-            Double rating,
+            int id,
+            int tmdb_id,
+            int user_id,
+            int total_like,
+            int total_comment,
+            boolean is_liked,
+            double rating,
             String review_detail,
             String timestamp,
             String title,
@@ -36,131 +32,83 @@ public class Review {
             String username,
             String profile_picture
     ) {
-        this.id = id;
-        this.tmdb_id = tmdb_id;
-        this.user_id = user_id;
-        this.total_like = total_like;
-        this.total_comment = total_comment;
-        this.is_liked = is_liked;
-        this.rating = rating;
-        this.review_detail = review_detail;
-        this.timestamp = timestamp;
-        this.title = title;
-        this.release_date = release_date;
-        this.poster = poster;
-        this.username = username;
-        this.profile_picture = profile_picture;
+        mId = id;
+        mTmdb_id = tmdb_id;
+        mUser_id = user_id;
+        mTotal_like = total_like;
+        mTotal_comment = total_comment;
+        mIs_liked = is_liked;
+        mRating = rating;
+        mReview_detail = review_detail;
+        mTimestamp = timestamp;
+        mTitle = title;
+        mRelease_date = release_date;
+        mPoster = poster;
+        mUsername = username;
+        mProfile_picture = profile_picture;
     }
 
-    public Integer getId() {
-        return id;
+    public int getId() {
+        return mId;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public int getTmdb_id() {
+        return mTmdb_id;
     }
 
-    public Integer getTmdb_id() {
-        return tmdb_id;
+    public int getUser_id() {
+        return mUser_id;
     }
 
-    public void setTmdb_id(Integer tmdb_id) {
-        this.tmdb_id = tmdb_id;
+    public int getTotal_like() {
+        return mTotal_like;
     }
 
-    public Integer getUser_id() {
-        return user_id;
+    public void setTotal_like(int total_like) {
+        mTotal_like = total_like;
     }
 
-    public void setUser_id(Integer user_id) {
-        this.user_id = user_id;
+    public int getTotal_comment() {
+        return mTotal_comment;
     }
 
-    public Integer getTotal_like() {
-        return total_like;
+    public boolean getIs_liked() {
+        return mIs_liked;
     }
 
-    public void setTotal_like(Integer total_like) {
-        this.total_like = total_like;
+    public void setIs_liked(boolean is_liked) {
+        mIs_liked = is_liked;
     }
 
-    public Integer getTotal_comment() {
-        return total_comment;
-    }
-
-    public void setTotal_comment(Integer total_comment) {
-        this.total_comment = total_comment;
-    }
-
-    public Boolean getIs_liked() {
-        return is_liked;
-    }
-
-    public void setIs_liked(Boolean is_liked) {
-        this.is_liked = is_liked;
-    }
-
-    public Double getRating() {
-        return rating;
-    }
-
-    public void setRating(Double rating) {
-        this.rating = rating;
+    public double getRating() {
+        return mRating;
     }
 
     public String getReview_detail() {
-        return review_detail;
-    }
-
-    public void setReview_detail(String review_detail) {
-        this.review_detail = review_detail;
+        return mReview_detail;
     }
 
     public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
+        return mTimestamp;
     }
 
     public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
+        return mTitle;
     }
 
     public String getRelease_date() {
-        return release_date;
-    }
-
-    public void setRelease_date(String release_date) {
-        this.release_date = release_date;
+        return mRelease_date;
     }
 
     public String getPoster() {
-        return poster;
-    }
-
-    public void setPoster(String poster) {
-        this.poster = poster;
+        return mPoster;
     }
 
     public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+        return mUsername;
     }
 
     public String getProfile_picture() {
-        return profile_picture;
-    }
-
-    public void setProfile_picture(String profile_picture) {
-        this.profile_picture = profile_picture;
+        return mProfile_picture;
     }
 }
