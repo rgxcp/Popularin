c64229529d0ca702ea4306464a302bbcb43c7200
Rommy Gustiawan <rgcppp@gmail.com>
2020-06-21 19:07:48 +0700

    Refaktor constructor dalam adapter film

--- app/src/main/java/xyz/fairportstudios/popularin/adapters/FilmAdapter.java
+++ app/src/main/java/xyz/fairportstudios/popularin/adapters/FilmAdapter.java
@@ -1,7 +1,6 @@
 package xyz.fairportstudios.popularin.adapters;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,114 +8,101 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
 import xyz.fairportstudios.popularin.R;
-import xyz.fairportstudios.popularin.activities.FilmDetailActivity;
-import xyz.fairportstudios.popularin.modals.FilmModal;
 import xyz.fairportstudios.popularin.models.Film;
 import xyz.fairportstudios.popularin.services.ParseDate;
 import xyz.fairportstudios.popularin.services.ConvertGenre;
-import xyz.fairportstudios.popularin.statics.Popularin;
 import xyz.fairportstudios.popularin.statics.TMDbAPI;
 
 public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.FilmViewHolder> {
-    private Context context;
-    private List<Film> filmList;
+    private Context mContext;
+    private List<Film> mFilmList;
+    private OnClickListener mOnClickListener;
 
-    public FilmAdapter(Context context, List<Film> filmList) {
-        this.context = context;
-        this.filmList = filmList;
+    public FilmAdapter(Context context, List<Film> filmList, OnClickListener onClickListener) {
+        mContext = context;
+        mFilmList = filmList;
+        mOnClickListener = onClickListener;
+    }
+
+    public interface OnClickListener {
+        void onItemClick(int position);
+
+        void onPosterClick(int position);
+
+        void onPosterLongClick(int position);
     }
 
     @NonNull
     @Override
     public FilmViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new FilmViewHolder(LayoutInflater.from(context).inflate(R.layout.recycler_film, parent, false));
+        return new FilmViewHolder(LayoutInflater.from(mContext).inflate(R.layout.recycler_film, parent, false), mOnClickListener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull FilmViewHolder holder, int position) {
         // Posisi
-        Film currentItem = filmList.get(position);
-
-        // Extra
-        final Integer filmID = currentItem.getId();
+        Film currentItem = mFilmList.get(position);
 
         // Parsing
-        final String filmTitle = currentItem.getOriginal_title();
-        final String filmYear = new ParseDate().getYear(currentItem.getRelease_date());
-        final String filmPoster = TMDbAPI.BASE_SMALL_IMAGE_URL + currentItem.getPoster_path();
-        final String filmGenre = new ConvertGenre().getGenreForHumans(currentItem.getGenre_id());
-        final String filmReleaseDate = new ParseDate().getDateForHumans(currentItem.getRelease_date());
-
-        // Request gambar
-        RequestOptions requestOptions = new RequestOptions()
-                .centerCrop()
-                .placeholder(R.color.colorSurface)
-                .error(R.color.colorSurface);
+        String genre = new ConvertGenre().getGenreForHumans(currentItem.getGenre_id());
+        String releaseDate = new ParseDate().getDateForHumans(currentItem.getRelease_date());
+        String poster = TMDbAPI.BASE_SMALL_IMAGE_URL + currentItem.getPoster_path();
 
         // Isi
-        holder.textFilmTitle.setText(filmTitle);
-        holder.textFilmGenre.setText(filmGenre);
-        holder.textFilmReleaseDate.setText(filmReleaseDate);
-        Glide.with(context).load(filmPoster).apply(requestOptions).into(holder.imageFilmPoster);
-
-        // Activity
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, FilmDetailActivity.class);
-                intent.putExtra(Popularin.FILM_ID, filmID);
-                context.startActivity(intent);
-            }
-        });
-
-        holder.imageFilmPoster.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, FilmDetailActivity.class);
-                intent.putExtra(Popularin.FILM_ID, filmID);
-                context.startActivity(intent);
-            }
-        });
-
-        holder.imageFilmPoster.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
-                FilmModal filmModal = new FilmModal(filmID, filmTitle, filmYear, filmPoster);
-                filmModal.show(fragmentManager, Popularin.FILM_MODAL);
-                return true;
-            }
-        });
+        holder.mTextTitle.setText(currentItem.getOriginal_title());
+        holder.mTextGenre.setText(genre);
+        holder.mTextReleaseDate.setText(releaseDate);
+        Glide.with(mContext).load(poster).into(holder.mImagePoster);
     }
 
     @Override
     public int getItemCount() {
-        return filmList.size();
+        return mFilmList.size();
     }
 
-    static class FilmViewHolder extends RecyclerView.ViewHolder {
-        private ImageView imageFilmPoster;
-        private TextView textFilmTitle;
-        private TextView textFilmGenre;
-        private TextView textFilmReleaseDate;
+    static class FilmViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+        private ImageView mImagePoster;
+        private TextView mTextTitle;
+        private TextView mTextGenre;
+        private TextView mTextReleaseDate;
+        private OnClickListener mOnClickListener;
 
-        FilmViewHolder(@NonNull View itemView) {
+        FilmViewHolder(@NonNull View itemView, OnClickListener onClickListener) {
             super(itemView);
 
-            imageFilmPoster = itemView.findViewById(R.id.image_rf_poster);
-            textFilmTitle = itemView.findViewById(R.id.text_rf_title);
-            textFilmGenre = itemView.findViewById(R.id.text_rf_genre);
-            textFilmReleaseDate = itemView.findViewById(R.id.text_rf_release_date);
+            mImagePoster = itemView.findViewById(R.id.image_rf_poster);
+            mTextTitle = itemView.findViewById(R.id.text_rf_title);
+            mTextGenre = itemView.findViewById(R.id.text_rf_genre);
+            mTextReleaseDate = itemView.findViewById(R.id.text_rf_release_date);
+            mOnClickListener = onClickListener;
+
+            itemView.setOnClickListener(this);
+            mImagePoster.setOnClickListener(this);
+            mImagePoster.setOnLongClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v == itemView) {
+                mOnClickListener.onItemClick(getAdapterPosition());
+            } else if (v == mImagePoster) {
+                mOnClickListener.onPosterClick(getAdapterPosition());
+            }
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            if (v == mImagePoster) {
+                mOnClickListener.onPosterLongClick(getAdapterPosition());
+            }
+            return true;
         }
     }
 }
