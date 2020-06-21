6edeca1a996d4f5668a0f76d0ed8ef0086c69c35
Rommy Gustiawan <rgcppp@gmail.com>
2020-06-21 13:04:30 +0700

    Mengubah tipe data menjadi primitive & menghapus setter yang tidak digunakan dalam model film review

--- app/src/main/java/xyz/fairportstudios/popularin/models/FilmReview.java
+++ app/src/main/java/xyz/fairportstudios/popularin/models/FilmReview.java
@@ -1,122 +1,86 @@
 package xyz.fairportstudios.popularin.models;
 
 public class FilmReview {
-    private Integer id;
-    private Integer user_id;
-    private Integer total_like;
-    private Integer total_comment;
-    private Boolean is_liked;
-    private Double rating;
-    private String review_detail;
-    private String timestamp;
-    private String username;
-    private String profile_picture;
-
-    public FilmReview() {
-        // Constructor kosong
-    }
+    private int mId;
+    private int mUser_id;
+    private int mTotal_like;
+    private int mTotal_comment;
+    private boolean mIs_liked;
+    private double mRating;
+    private String mReview_detail;
+    private String mTimestamp;
+    private String mUsername;
+    private String mProfile_picture;
 
     public FilmReview(
-            Integer id,
-            Integer user_id,
-            Integer total_like,
-            Integer total_comment,
-            Boolean is_liked,
-            Double rating,
+            int id,
+            int user_id,
+            int total_like,
+            int total_comment,
+            boolean is_liked,
+            double rating,
             String review_detail,
             String timestamp,
             String username,
             String profile_picture
     ) {
-        this.id = id;
-        this.user_id = user_id;
-        this.total_like = total_like;
-        this.total_comment = total_comment;
-        this.is_liked = is_liked;
-        this.rating = rating;
-        this.review_detail = review_detail;
-        this.timestamp = timestamp;
-        this.username = username;
-        this.profile_picture = profile_picture;
+        mId = id;
+        mUser_id = user_id;
+        mTotal_like = total_like;
+        mTotal_comment = total_comment;
+        mIs_liked = is_liked;
+        mRating = rating;
+        mReview_detail = review_detail;
+        mTimestamp = timestamp;
+        mUsername = username;
+        mProfile_picture = profile_picture;
     }
 
-    public Integer getId() {
-        return id;
+    public int getId() {
+        return mId;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public int getUser_id() {
+        return mUser_id;
     }
 
-    public Integer getUser_id() {
-        return user_id;
+    public int getTotal_like() {
+        return mTotal_like;
     }
 
-    public void setUser_id(Integer user_id) {
-        this.user_id = user_id;
+    public void setTotal_like(int total_like) {
+        mTotal_like = total_like;
     }
 
-    public Integer getTotal_like() {
-        return total_like;
+    public int getTotal_comment() {
+        return mTotal_comment;
     }
 
-    public void setTotal_like(Integer total_like) {
-        this.total_like = total_like;
+    public boolean getIs_liked() {
+        return mIs_liked;
     }
 
-    public Integer getTotal_comment() {
-        return total_comment;
+    public void setIs_liked(boolean is_liked) {
+        mIs_liked = is_liked;
     }
 
-    public void setTotal_comment(Integer total_comment) {
-        this.total_comment = total_comment;
-    }
-
-    public Boolean getIs_liked() {
-        return is_liked;
-    }
-
-    public void setIs_liked(Boolean is_liked) {
-        this.is_liked = is_liked;
-    }
-
-    public Double getRating() {
-        return rating;
-    }
-
-    public void setRating(Double rating) {
-        this.rating = rating;
+    public double getRating() {
+        return mRating;
     }
 
     public String getReview_detail() {
-        return review_detail;
-    }
-
-    public void setReview_detail(String review_detail) {
-        this.review_detail = review_detail;
+        return mReview_detail;
     }
 
     public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
+        return mTimestamp;
     }
 
     public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+        return mUsername;
     }
 
     public String getProfile_picture() {
-        return profile_picture;
-    }
-
-    public void setProfile_picture(String profile_picture) {
-        this.profile_picture = profile_picture;
+        return mProfile_picture;
     }
 }
